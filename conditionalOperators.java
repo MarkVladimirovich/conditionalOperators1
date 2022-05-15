218ce907cf81dd0a816b614c4cd7a77560ae6212
Mark Shvendykh <mmordikov@mail.ru>
2022-05-15 11:14:00 +0300

    1

--- conditionalOperators.java
+++ conditionalOperators.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class conditionalOperators {
+public class conditionalOperators {
+    public static void conditionalOperators(String[] args) {
+        int clientOS = 1;
+        if (clientOS = 1) {
+            system.out.println(Установите версию приложения для Android по ссылке);
+
+        }
+    }
 }
